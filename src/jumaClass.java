591e070e92bf632df2d21f75681761f86402df80
juma <jumajuma@MacBook-Air-juma.local>
2025-04-21 20:21:48 -0500

    added new class

--- src/jumaClass.java
+++ src/jumaClass.java
@@ -2,5 +2,7 @@ public class jumaClass {
     public static void main(String[] args) {
         System.out.println("jumaClass.main");
         System.out.println("lets discuss, because I dont understand");
+        System.out.println("Ass" +
+                "");
     }
 }
