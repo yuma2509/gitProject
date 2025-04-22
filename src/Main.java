50eebaa66c7c20e8910a89f15ef5e4b691564c0a
juma <jumajuma@MacBook-Air-juma.local>
2025-04-21 20:08:05 -0500

    added new class

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println("Hi world");
+        for (int i = 0; i < 11; i++) {
+            System.out.println("Hi" +
+                    "bnbnn" +
+                    "hbhbjnjknj");
+        }
     }
+
 }
---
+++ src/jumaClass.java
@@ -0,0 +1,6 @@
+public class jumaClass {
+    public static void main(String[] args) {
+        System.out.println("jumaClass.main");
+        System.out.println("lets discuss, because I dont understand");
+    }
+}
