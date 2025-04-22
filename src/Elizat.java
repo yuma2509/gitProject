7b88efdae4bbdd54d9934b8aeabe4f10aa4ad60d
Elizat25 <elizat.tashpolotova@gmail.com>
2025-04-21 20:46:59 -0500

    changes in Elizat class

--- src/Elizat.java
+++ src/Elizat.java
@@ -1,5 +1,5 @@
 public class Elizat {
     public static void main(String[] args) {
-        System.out.println("Hi everyone!Good luck everyone!");
+        System.out.println("Hi everyone!Good evening");
     }
 }
